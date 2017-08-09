67a96495aa55c31a07b0a5a2ce971d868236beab
zacksleo <zacksleo@gmail.com>
2017-08-09 23:07:44 +0800

    update test

--- tests/WeuiJsAssetTest.php
+++ tests/WeuiJsAssetTest.php
@@ -16,6 +16,11 @@ class WeuiJsAssetTest extends TestCase
         $view = $this->getView();
         $this->assertEmpty($view->assetBundles);
         WeuiJsAsset::register($view);
+        $asset = new WeuiAsset();
+        $this->assertEquals('@npm/weui.js', $asset->sourcePath);
+        $this->assertArrayHasKey('only', $asset->publishOptions);
+        $this->assertAttributeEquals(['dist/weui.min.js'], 'js', $asset);
+        $this->assertAttributeEquals(['zacksleo\yii2\weui\assets\WeuiAsset'], 'depends', $asset);
         $this->assertEquals(2, count($view->assetBundles));
         $this->assertArrayHasKey('zacksleo\\yii2\\weui\\assets\\WeuiJsAsset', $view->assetBundles);
         $this->assertArrayHasKey('zacksleo\\yii2\\weui\\assets\\WeuiAsset', $view->assetBundles);
--- tests/data/views/layout.php
+++ tests/data/views/layout.php
@@ -11,7 +11,7 @@
     </head>
     <body>
     <?php $this->beginBody(); ?>
-    
+
     <?php $this->endBody(); ?>
     </body>
     </html>
