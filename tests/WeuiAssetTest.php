dfc5a474293c2a17c065521d83b816d9f681330e
zacksleo <zacksleo@gmail.com>
2017-08-09 23:15:21 +0800

    remove attribute test

--- tests/WeuiAssetTest.php
+++ tests/WeuiAssetTest.php
@@ -16,10 +16,6 @@ class WeuiAssetTest extends TestCase
         $view = $this->getView();
         $this->assertEmpty($view->assetBundles);
         WeuiAsset::register($view);
-        $asset = new WeuiAsset();
-        $this->assertEquals('@npm/weui', $asset->sourcePath);
-        $this->assertArrayHasKey('only', $asset->publishOptions);
-        $this->assertAttributeEquals(['dist/style/weui.min.css'], 'css', $asset);
         $this->assertEquals(1, count($view->assetBundles));
         $this->assertArrayHasKey('zacksleo\\yii2\\weui\\assets\\WeuiAsset', $view->assetBundles);
         $this->assertTrue($view->assetBundles['zacksleo\\yii2\\weui\\assets\\WeuiAsset'] instanceof AssetBundle);
