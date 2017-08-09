0697b0b106eb3f7a0fdab09e48d4e4018f139000
zacksleo <zacksleo@gmail.com>
2017-08-09 21:52:15 +0800

    update test

---
+++ tests/WeuiAssetTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @link https://github.com/2amigos/yii2-dosamigos-asset-bundle
+ * @copyright Copyright (c) 2013-2015 2amigOS! Consulting Group LLC
+ * @license http://opensource.org/licenses/BSD-3-Clause
+ */
+
+namespace tests;
+
+use zacksleo\yii2\weui\assets\WeuiAsset;
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * DosAmigosAssetTest
+ */
+class DosAmigosAssetTest extends TestCase
+{
+    public function testAsset()
+    {
+        $view = $this->getView();
+        $this->assertEmpty($view->assetBundles);
+        WeuiAsset::register($view);
+        $this->assertEquals(2, count($view->assetBundles));
+        $this->assertArrayHasKey('zacksleo\\yii2\\assets\WeuiAsset', $view->assetBundles);
+        $this->assertTrue($view->assetBundles['zacksleo\\yii2\\weui\assets\WeuiAsset'] instanceof AssetBundle);
+        $content = $view->renderFile('@tests/data/views/layout.php');
+        $this->assertContains('jquery.js', $content);
+        $this->assertContains('dosamigos.js', $content);
+    }
+}
